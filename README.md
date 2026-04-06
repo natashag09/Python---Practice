e5eac8c86a8d67559e9f2ad5bd47a169ce9c8957
natashag09 <natashag8292@gmail.com>
2026-04-07 03:54:06 +0530

    Day 1 Python Practice - Basics
    Added basic programs including variables, type function, input and print statements .

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # Python---Practice
 This repository contains my daily Python practice focused on building a strong foundation for data analysis and problem solving.
+
+## Day 1 
+Topics Covered:
+- Print Fucntion 
+- Variables 
+- Type Function 
+- Input Function 
+
+Practiced basic python programs including variable declaration, taking user input , checking the datatype , and printing output.
+
+I will update this repository daily with new topics.
---
+++ day1.py
@@ -0,0 +1,50 @@
+# Basics 
+
+# Print Function 
+
+#Q Print Hello World 
+print('Hello World')
+print("Hello World")
+
+#Q Display Hello 'World' 
+print("Hello 'World' ")
+
+
+# Variables 
+
+#Q Print name and age 
+
+name = "Natasha"
+age = 23
+print(name, age)
+
+#Q Print the value of a 
+a = 10
+print('The value of a is',a)
+
+
+# Datatypes (Type Function)
+
+#Q Check the data type of s = 45
+s = 45
+print('The datatype of',s,'is', type(s))
+
+#Q Check the datatype of d = False
+d = False 
+print('The datatype of',d,'is', type(d))
+
+
+# Input Function 
+
+#Q Take a as an input and print it
+a = int(input('enter the value of a :'))
+print(a)
+
+#Q Take b as an input and print b and the datatype of b 
+b = int(input('The value of b:'))
+print(b, type(b))
+
+#Q Take marks input and print the marks
+marks = float(input('enter your marks:'))
+print('My marks are:', marks)
+
